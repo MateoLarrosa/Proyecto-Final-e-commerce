2f34087fff5c63e13c99538ce0da744572423604
Andrés Felipe Méndez Florez <andres.f.mendez123@gmail.com>
2025-04-27 13:01:58 -0300

    Cambio de componente funcional de flecha a uno normal, similar a lo implementado por Adolfo

--- PF_API_e-commerce/src/App.jsx
+++ PF_API_e-commerce/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import MiPerfilApp from './pages/miPerfil/MiPerfilApp'
 
-export const App = () => {
-  return (
-    <MiPerfilApp/>
-  )
+// export const App = () => {
+//   return (
+//     <MiPerfilApp/>
+//   )
+// }
+function App() {
+    return (
+        <MiPerfilApp/>
+    )
 }
+export default App
