059f125a3196c839ed2ee62e8450e7eb06bf3eb1
Andrés Felipe Méndez Florez <andres.f.mendez123@gmail.com>
2025-05-29 17:01:16 -0300

    Agrego un proyecto Spring Boot a la carpeta Backend para trabajar codigo java

---
+++ PF_API_e-commerce/Backend/src/main/java/com/api/amazonas/amazonas_e_commerce/AmazonasECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.api.amazonas.amazonas_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonasECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonasECommerceApplication.class, args);
+	}
+
+}
---
+++ PF_API_e-commerce/Backend/src/test/java/com/api/amazonas/amazonas_e_commerce/AmazonasECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.amazonas.amazonas_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonasECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
